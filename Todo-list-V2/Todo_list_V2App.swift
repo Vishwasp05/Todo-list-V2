b1eaa8823e4b13e855306790b50809c61b0b3890
Vishwas Sharma <ch0nky@protonmail.com>
2024-02-21 17:05:03 +0530

    Initial Commit

---
+++ Todo-list-V2/Todo_list_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_list_V2App.swift
+//  Todo-list-V2
+//
+//  Created by Vishwas Sharma on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_list_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
